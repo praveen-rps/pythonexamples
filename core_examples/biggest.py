740ce3482129634dac8504336c5f5575dd0c8fe3
Praveen <gs.praveenkumar5@gmail.com>
2023-12-02 06:53:19 +0530

    modified repeated string to constant

--- core_examples/biggest.py
+++ core_examples/biggest.py
@@ -1,16 +1,16 @@
 n1 = int(input("Enter first number"))
 n2 = int(input("Enter second number"))
 n3 = int(input("Enter third number"))
-
+str = " is biggest number"
 if n1 < n2:
     if n2 < n3:
-        print(n3," is biggest number")
+        print(n3,str)
     else:
-        print(n2," is biggest number")
+        print(n2,str)
 else:
     if n1 < n3:
-        print(n3," is biggest number")
+        print(n3,str)
     else:
-        print(n1," is biggest number")
+        print(n1,str)
 
 
